759d95713007bfc1c2c73a90bcf2235ac2cf045a
Kirill Guselnikov <krllgslnkv@gmail.com>
2021-10-22 00:22:59 +0500

    Initial Commit

---
+++ Background Changer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Background Changer
+//
+//  Created by Kirill Guselnikov on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

81d0da644955473404e16c9ec7c16af20a5a311f
Kirill Guselnikov <krllgslnkv@gmail.com>
2021-10-24 15:35:55 +0500

    Sliders, view, labels

--- Background Changer/ViewController.swift
+++ Background Changer/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorViewChanger: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorViewChanger.layer.cornerRadius = 15
+        
+        redSlider.tintColor = UIColor.red
+        greenSlider.tintColor = UIColor.green
+        blueSlider.tintColor = UIColor.blue
+        
     }
 
-
+    func changeColor() {
+        colorViewChanger.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    @IBAction func redSliderAction(_ sender: Any) {
+        redValue.text = String(round(redSlider.value * 100) / 100)
+        changeColor()
+    }
+    @IBAction func greenSliderAction(_ sender: Any) {
+        greenValue.text = String(round(greenSlider.value * 100) / 100)
+        changeColor()
+    }
+    
+    @IBAction func blueSliderAction(_ sender: Any) {
+        blueValue.text = String(round(blueSlider.value * 100) / 100)
+        changeColor()
+    }
 }
 
+//разобраться с констрейнтами 
+//убрать нули в значениях слайдера - done
